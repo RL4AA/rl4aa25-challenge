c96f265578ef9764ea1f7a5c3a8652f99fc41a7e
Chenran Xu <chenran.xu@kit.edu>
2025-03-21 10:14:57 +0100

    Include both visualizations into the introduction notebook

---
+++ src/visualization.py
@@ -0,0 +1,150 @@
+import asyncio
+import logging
+
+import cv2
+import gymnasium as gym
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+from beam_3d_visualizer.beam_server.beam_visualization_wrapper import (
+    BeamVisualizationWrapper,
+)
+from src.environments import ea
+
+
+async def run_simulation_async(env: ea.TransverseTuning, logger: logging.Logger):
+    """Run the simulation loop, stepping the environment with control actions."""
+    print("\n--- Starting simulation loop ---")
+
+    step_count = 0
+    done = False
+
+    # Reset the environment
+    observation, _ = env.reset()
+
+    while not done:
+        # Render and broadcast data to clients
+        # Calls BeamVisualizationWrapper.render(),
+        # which delegates to WebSocketWrapper.render()
+        await env.render()
+
+        # Check if we have a new control action from WebSocket
+        if env.control_action is not None:
+            print(f"New action received: {env.control_action}")
+            env.last_action = env.control_action  # Update last_action with new action
+            env.control_action = None  # Clear after use
+
+        # Step through the environment with the last action
+        # Note: last_action persists and is reused if no new action is received
+        observation, reward, terminated, truncated, info = env.step(env.last_action)
+
+        # Update step count
+        step_count += 1
+
+        logger.info(
+            "Step %d: Action = %s, Reward = %s, Observation = %s",
+            step_count,
+            env.last_action,
+            reward,
+            observation,
+        )
+
+        done = truncated  # or terminated
+
+    env.close()
+    print("Simulation completed.")
+
+
+async def visualization_main(logger: logging.Logger):
+    """Main entry point to set up the environment and start the simulation."""
+    # Initialize the environment and wrap it with BeamVisualizationWrapper
+    env = ea.TransverseTuning(
+        backend="cheetah",
+        action_mode="direct",
+        magnet_init_mode=None,
+        render_mode="rgb_array",  # "human",
+        backend_args={"generate_screen_images": True},
+    )
+    env = BeamVisualizationWrapper(env)
+
+    if not env.env.connected:
+        print("Waiting for WebSocket client to connect...")
+        await asyncio.sleep(5.0)  # Small delay to prevent CPU overload
+
+    # Run the simulation
+    await run_simulation_async(env, logger=logger)
+
+    print("Simulation shutdown completed.")
+
+
+def restart_manual_tuning(env: gym.Env):
+    """Restart the manual tuning process and display in the jupyter notebook cell."""
+
+    assert isinstance(env.env, ea.TransverseTuning)
+
+    observation, info = env.reset()
+
+    # magnet_names = info["magnet_names"]
+    magnet_names = ["Q1", "Q2", "Cor_V", "Q3", "Cor_H"]
+    magnet_mins = env.observation_space["magnets"].low
+    magnet_maxs = env.observation_space["magnets"].high
+    current_magnet_settings = observation["magnets"]
+
+    magnet_mins[2] = magnet_mins[2] * 1e3
+    magnet_mins[4] = magnet_mins[4] * 1e3
+    magnet_maxs[2] = magnet_maxs[2] * 1e3
+    magnet_maxs[4] = magnet_maxs[4] * 1e3
+    current_magnet_settings[2] = current_magnet_settings[2] * 1e3
+    current_magnet_settings[4] = current_magnet_settings[4] * 1e3
+
+    image = env.render()
+
+    magnet_widgets = [
+        widgets.BoundedFloatText(
+            value=setting, description=name, min=min_limit, max=max_limit
+        )
+        for name, min_limit, max_limit, setting in zip(
+            magnet_names, magnet_mins, magnet_maxs, current_magnet_settings
+        )
+    ]
+    for w in magnet_widgets:
+        w.observe(lambda _: update_screen_image(), names="value")
+
+    done_button = widgets.Button(description="Done!")
+    done_button.on_click(lambda _: on_done_button_clicked())
+
+    bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    _, buffer = cv2.imencode(".png", bgr)
+    screen_image_widget = widgets.Image(
+        value=buffer, format="png", width=500, height=500
+    )
+
+    def update_screen_image():
+        action = [widget.value for widget in magnet_widgets]
+        action[2] = action[2] * 1e-3
+        action[4] = action[4] * 1e-3
+        observation, _, _, _, _ = env.step(action)
+
+        for i, (w, setting) in enumerate(zip(magnet_widgets, observation["magnets"])):
+            if i == 2 or i == 4:
+                w.value = setting * 1e3
+            else:
+                w.value = setting
+
+        image = env.render()
+        bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        _, buffer = cv2.imencode(".png", bgr)
+        screen_image_widget.value = buffer
+
+    def on_done_button_clicked():
+        for w in magnet_widgets:
+            w.close()
+        screen_image_widget.close()
+        done_button.close()
+        # Show the iamge one last time
+        _ = env.generate_episode_plot()
+        plt.show()
+        env.close()
+
+    display(*magnet_widgets, screen_image_widget, done_button)
